4fec8abecca3bd694d07668100f08dc2e9e62184
Triston Hine <hinetg@rose-hulman.edu>
2019-05-09 10:56:32 -0400

    heres more

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -7,19 +7,21 @@ This module demonstrates how to INPUT from the CONSOLE:
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.  Read and run this program.  Then do the following problems,
+# DONE: 2.  Read and run this program.  Then do the following problems,
 #   putting your answers RIGHT HERE IN THIS DOCUMENT.
 #
 #   1. Write a line of code that would input an INTEGER from the
 #      console, storing the integer in a variable called 'x'.
 #      Write your line here:
+#   x = int(input('here's your number i guess')
 #
 #   2. Write a line of code that would input an FLOAT from the console,
 #      storing the float in a variable called 'x'.
 #      Write your line here:
+#   x = float(input('here's a decimal i guess"))
 #
 #   3. Write a line of code that would input an STRING from the console,
 #      storing the string in a variable called 'x'.
